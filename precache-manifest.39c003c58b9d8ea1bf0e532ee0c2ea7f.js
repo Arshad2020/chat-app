3b6fac1927d7f89211ebe3ad4605d982adebad33
Arshad <arshad.researcher@gmail.com>
2020-07-16 11:52:40 +0200

    Updates

---
+++ precache-manifest.39c003c58b9d8ea1bf0e532ee0c2ea7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c92178b8e73e15c1aec10c565c0820c",
+    "url": "/Arshad2020/chat-app.git/index.html"
+  },
+  {
+    "revision": "fc520a008c292001da31",
+    "url": "/Arshad2020/chat-app.git/static/css/main.32d4d637.chunk.css"
+  },
+  {
+    "revision": "4f153353f70aeab50e9c",
+    "url": "/Arshad2020/chat-app.git/static/js/2.03c7445f.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/Arshad2020/chat-app.git/static/js/2.03c7445f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc520a008c292001da31",
+    "url": "/Arshad2020/chat-app.git/static/js/main.65839059.chunk.js"
+  },
+  {
+    "revision": "e275e28969056cb92fba",
+    "url": "/Arshad2020/chat-app.git/static/js/runtime-main.13b65eb0.js"
+  }
+]);
